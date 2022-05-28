59b04c64407659a7864877fec3ae2df4993b910e
Mike McBride <mike.mcbride@hey.com>
2022-05-28 13:39:39 -0500

    suggested dates mobile tweaks

--- src/components/RankedResults.vue
+++ src/components/RankedResults.vue
@@ -1,56 +1,60 @@
 <template>
     <div>
         <h3 class="mt-8 mb-4 text-2xl font-bold">Suggested Dates</h3>
-        <section
-            class="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg divide-y divide-gray-200 bg-white"
-        >
-            <div
-                v-for="row in responsesPerQuestion"
-                :key="row.date"
-                class="p-4 md:py-6 lg:py-8 lg:px-6 flex items-center"
-            >
-                <div
-                    class="font-bold text-sm md:text-base flex-shrink-0 text-right pr-4 md:pr-6 lg:pr-8"
+        <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+            <div class="py-2 md:px-6 lg:px-8">
+                <section
+                    class="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg divide-y divide-gray-200 bg-white"
                 >
-                    {{ row.date }}
-                </div>
-                <div class="flex h-6 flex-shrink rounded w-full">
                     <div
-                        v-if="row.yes > 0"
-                        class="group relative bg-emerald-500"
-                        :class="[row.yes > 0 ? 'rounded-l' : '', row.yes === row.total ? 'rounded-r' : '']"
-                        :style="`width: ${(row.yes / row.total) * 100}%;`"
+                        v-for="row in responsesPerQuestion"
+                        :key="row.date"
+                        class="p-4 md:py-6 lg:py-8 lg:px-6 flex items-center"
                     >
-                        <span
-                            class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
-                            >{{ row.yes }}</span
+                        <div
+                            class="font-bold text-sm md:text-base flex-shrink-0 text-right pr-4 md:pr-6 lg:pr-8"
                         >
+                            {{ row.date }}
+                        </div>
+                        <div class="flex h-6 flex-shrink rounded w-full">
+                            <div
+                                v-if="row.yes > 0"
+                                class="group relative bg-emerald-500"
+                                :class="[row.yes > 0 ? 'rounded-l' : '', row.yes === row.total ? 'rounded-r' : '']"
+                                :style="`width: ${(row.yes / row.total) * 100}%;`"
+                            >
+                                <span
+                                    class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
+                                    >{{ row.yes }}</span
+                                >
+                            </div>
+                            <div
+                                v-if="row.if_needed > 0"
+                                class="group relative bg-yellow-400"
+                                :class="[row.yes === 0 ? 'rounded-l' : '', row.no === 0 ? 'rounded-r' : '']"
+                                :style="`width: ${(row.if_needed / row.total) * 100}%;`"
+                            >
+                                <span
+                                    class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
+                                    >{{ row.if_needed }}</span
+                                >
+                            </div>
+                            <div
+                                v-if="row.no > 0"
+                                class="group relative bg-red-500"
+                                :class="[row.no > 0 ? 'rounded-r' : '', row.no === row.total ? 'rounded-l' : '']"
+                                :style="`width: ${(row.no / row.total) * 100}%;`"
+                            >
+                                <span
+                                    class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
+                                    >{{ row.no }}</span
+                                >
+                            </div>
+                        </div>
                     </div>
-                    <div
-                        v-if="row.if_needed > 0"
-                        class="group relative bg-yellow-400"
-                        :class="[row.yes === 0 ? 'rounded-l' : '', row.no === 0 ? 'rounded-r' : '']"
-                        :style="`width: ${(row.if_needed / row.total) * 100}%;`"
-                    >
-                        <span
-                            class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
-                            >{{ row.if_needed }}</span
-                        >
-                    </div>
-                    <div
-                        v-if="row.no > 0"
-                        class="group relative bg-red-500"
-                        :class="[row.no > 0 ? 'rounded-r' : '', row.no === row.total ? 'rounded-l' : '']"
-                        :style="`width: ${(row.no / row.total) * 100}%;`"
-                    >
-                        <span
-                            class="hidden group-hover:block px-2 py-1 rounded text-xs bg-gray-700 bg-opacity-90 text-white font-medium absolute -top-8 left-1/2 transform -translate-x-1/2"
-                            >{{ row.no }}</span
-                        >
-                    </div>
-                </div>
+                </section>
             </div>
-        </section>
+        </div>
     </div>
 </template>
 
