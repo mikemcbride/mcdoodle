1ea83882a943e4b9f84ab363ef6e17bb1fb56126
Mike McBride <mike.mcbride@hey.com>
2024-05-20 23:38:30 -0500

    update success message on verify page

--- pages/verify-account.js
+++ pages/verify-account.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import http from '../services/http';
 import { useEffect, useState } from 'react';
 import LoginError from '../components/LoginError.js';
+import { CheckCircleIcon } from '@heroicons/react/20/solid';
 
 export default function VerifyAccount({ verification, email, action }) {
   const [error, setError] = useState('');
@@ -55,8 +56,27 @@ export default function VerifyAccount({ verification, email, action }) {
           </div>
         )}
         {showSuccess && (
-          <div className="rounded-md bg-emerald-100 p-4">
-            <h3 className="text-sm font-medium text-emerald-800">Your account verification was successfully processed! Please login to continue. <Link href="/login" className="underline">Go to login.</Link></h3>
+          <div className="rounded-md bg-green-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
+              </div>
+              <div className="ml-3">
+                <h3 className="text-sm font-medium text-green-800">Success</h3>
+                <div className="mt-2 text-sm text-green-700">
+                  <p>Your account verification was successfully processed. Please login to continue.</p>
+                </div>
+                <div className="mt-4">
+                  <div className="-mx-2 -my-1.5 flex">
+                    <Link
+                      href="/login"
+                      type="button"
+                      className="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                    >Go to login&nbsp;<span aria-hidden="true"> &rarr;</span></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
